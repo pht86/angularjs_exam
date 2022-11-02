8419e139aec710f67a4efb2c1f73d27ebdf3521f
pht86 <114332758+pht86@users.noreply.github.com>
2022-11-02 15:46:47 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 var myApp = angular.module('productApp', []);
 myApp.controller('productCtrl', function ($scope){
     var products = [
-        {name: "Product 1", price: 15000000},
-        {name: "Product 1", price: 15000000},
-        {name: "Product 1", price: 15000000},
-        {name: "Product 1", price: 15000000},
-        {name: "Product 1", price: 15000000},
-        {name: "Product 1", price: 15000000}
+        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
+        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000},
+        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
+        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
+        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000},
+        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000}
     ]
     $scope.products = products;
+    $scope.addProduct = function(){
+        $scope.products.push($scope.newProduct);
+        $scope.newProduct = {};
+        $scope.message = "New product added successful!";
+    };
+    $scope.editProduct = function(){
+        $scope.message = "Edit product information sucessfuly!";
+    };
+    $scope.selectProduct = function(product){
+        console.log(product);
+        $scope.clickedProduct = product;
+    };
+    $scope.deleteProduct = function(){
+        $scope.products.splice($scope.products.indexOf($scope.clickedProduct), 1);
+        $scope.message = "Product deleted successful!";
+    };
+    $scope.clearMessage = function(){
+        $scope.message = "";
+    };
 })
+myApp.filter('status', function (){
+    return function (input) {
+        if (input == 0) return "Not Active";
+        if (input == 1) return "Active";
+    }
+});
