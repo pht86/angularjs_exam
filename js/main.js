ad82d7aa473f0abe37346515d43b036d5a42bbc0
pht86 <114332758+pht86@users.noreply.github.com>
2022-11-02 15:54:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var myApp = angular.module('productApp', []);
 myApp.controller('productCtrl', function ($scope){
     var products = [
-        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
-        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000},
-        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
-        {name: "Product 1",category: "Smart Phone",status:1, price: 15000000},
-        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000},
-        {name: "Product 1",category: "Smart Phone",status:0, price: 15000000}
+        {name: "Xiaomi 12T Pro",category: "Smart Phone",status:1, price: 16990000},
+        {name: "Laptop Asus Gaming Rog Strix G15 G513IH HN015W",category: "Laptop",status:0, price: 18690000},
+        {name: "Apple Mac mini M1 256GB 2020",category: "Laptop",status:1, price: 17990000},
+        {name: "iPhone 14 Pro 128GB",category: "Smart Phone",status:1, price: 30990000},
+        {name: "Samsung Galaxy S22 Ultra (8GB - 128GB)",category: "Smart Phone",status:0, price: 24890000},
+        {name: "ASUS ROG Phone 6 16GB 512GB",category: "Smart Phone",status:0, price: 24240000}
     ]
     $scope.products = products;
     $scope.addProduct = function(){
